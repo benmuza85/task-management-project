c371b9cc3cdbcb6c0c92586c5c82194a884a54df
Amos <amosmakupe8@gmail.com>
2024-08-29 11:44:24 +0200

    Second commit

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use('/auth', require('./routes/authRoutes'));
 app.use('/api', taskRoutes);
 
 app.use(express.static(path.join(__dirname,'client', 'build')))
-app.get('*', (req, res)=>{
+app.get('/*', (req, res)=>{
     res.sendFile(path.join(__dirname,'client','build','index.html'), (err)=>{
         if(err){
             res.status(500).send(err)
