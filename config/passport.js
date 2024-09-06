98ccdb979f8fba8abb5cc98519173ddc91ac6c9e
Amos <amosmakupe8@gmail.com>
2024-09-06 17:37:45 +0200

    google auth redirect url

--- config/passport.js
+++ config/passport.js
@@ -48,7 +48,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
+      callbackURL: 'https://task-management-benson-cda7d51e8621.herokuapp.com/auth/google/callback',
       scope:["profile", "email"]
     },
     async (token, tokenSecret, profile, done) => {
@@ -71,33 +71,5 @@ passport.use(
   )
 );
 
-// Facebook Strategy
-// passport.use(
-//   new FacebookStrategy(
-//     {
-//       clientID: process.env.FACEBOOK_APP_ID,
-//       clientSecret: process.env.FACEBOOK_APP_SECRET,
-//       callbackURL: '/auth/facebook/callback',
-//       profileFields: ['id', 'emails', 'name'],
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       try {
-//         let user = await User.findOne({ facebookId: profile.id });
-
-//         if (!user) {
-//           user = await User.create({
-//             facebookId: profile.id,
-//             email: profile.emails[0].value,
-//             name: `${profile.name.givenName} ${profile.name.familyName}`,
-//           });
-//         }
-
-//         return done(null, user);
-//       } catch (err) {
-//         return done(err, null);
-//       }
-//     }
-//   )
-// );
 
 module.exports = passport;
